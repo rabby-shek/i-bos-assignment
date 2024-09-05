990852bae4bab559f831e89055c0e2e52c23f13a
rabby-shek <rabbyshek1998@gmail.com>
2024-09-05 23:42:47 +0600

    folder structure created and router package installed

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,8 @@
-import React from 'react'
-
+import React from "react";
+import { RouterProvider } from "react-router-dom";
+import router from "./routers/Routes";
 const App = () => {
-  return (
-    <div>
-      app
-    </div>
-  )
-}
+  return <RouterProvider router={router} />;
+};
 
 export default App;
---
+++ src/components/layout/AuthLayout.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import { Outlet } from "react-router-dom";
+const AuthLayout = () => {
+  return <Outlet />;
+};
+
+export default AuthLayout;
